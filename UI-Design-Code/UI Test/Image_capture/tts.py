27e8b5f80a13cb94b8a0c1cb6891b9f4cdd7a5f4
pearson384 <53051413+pearson384@users.noreply.github.com>
2021-04-07 11:24:51 +0530

    Added .py file and Audio file for Text to Speech

---
+++ UI-Design-Code/UI Test/Image_capture/tts.py
@@ -0,0 +1,7 @@
+#first install the playsound library in cmd using **"pip install playsound"**
+#Python code and the audio fule must be in the same directory or else specific path must be satisfied
+
+from playsound import playsound
+  
+# for playing UI_AUDIO_01.wav file
+playsound('UI_AUDIO_01.wav')
